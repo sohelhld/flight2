1683ccb238aae109baa86b717ccb2b2197028904
Sohel Rana <112401379+sohelhld@users.noreply.github.com>
2023-08-14 18:49:08 +0530

    Update user.router.js

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/users.model');
 const userRouter = express.Router();
